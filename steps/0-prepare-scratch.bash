14ddc3bf20e6a85425b86b387433d91806ac4f10
Edward S. Rice <edsrice@gmail.com>
2023-04-24 13:28:22 +0200

    Use library names in input instead of paths

--- steps/0-prepare-scratch.bash
+++ steps/0-prepare-scratch.bash
@@ -8,25 +8,16 @@ source ${SCRIPT_DIR}/../config.sh
 reads_dir="$SCRATCH_DIR/reads"
 mkdir -p $reads_dir
 while read line; do
-    library_id=$(echo "$line" | cut -f1)
-    r1_reads=$(echo "$line" | cut -f2)
-    r2_reads=$(echo "$line" | cut -f3)
+    library_id=$line
 
     echo "copying: $library_id" 1>&2
 
-    newloc_r1_reads="$reads_dir/${library_id}_R1.fastq.gz"
-    newloc_r2_reads="$reads_dir/${library_id}_R2.fastq.gz"
-
-    if [[ $r1_reads == *","* ]]; then
-        cat $(echo $r1_reads | tr ',' ' ') > $newloc_r1_reads
-        cat $(echo $r2_reads | tr ',' ' ') > $newloc_r2_reads
-    else
-        cp $r1_reads $newloc_r1_reads
-        cp $r2_reads $newloc_r2_reads
-    fi
-
-    echo -e "$library_id\t$newloc_r1_reads\t$newloc_r2_reads"
-done < $LIBRARIES_LIST > $SCRATCH_DIR/libraries.tsv
+    cat $(find $LIBRARIES_DIR -name "${library_id}*_1.fq.gz") \
+        > "$reads_dir/${library_id}_R1.fastq.gz"
+    cat $(find $LIBRARIES_DIR -name "${library_id}*_2.fq.gz") \
+        > "$reads_dir/${library_id}_R2.fastq.gz"
+done < $LIBRARIES_LIST
+cp $LIBRARIES_LIST $SCRATCH_DIR/libraries.txt
 
 mkdir -p $SCRATCH_DIR/out/gam
 mkdir -p $SCRATCH_DIR/out/bam
